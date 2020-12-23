f37bb91abfce7dd58104241e7d0209d3cec922eb
anisbhati <anisbhati101@gmail.com>
2020-12-23 21:59:15 +0530

    4 th comment

--- src/main/java/com/anisbatcgdemo/controller/DataserviceController.java
+++ src/main/java/com/anisbatcgdemo/controller/DataserviceController.java
@@ -1,8 +1,14 @@
 package com.anisbatcgdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.anisbatcgdemo.repository.PersonRepository;
 
 @RestController
 public class DataserviceController {
-
+     
+	@Autowired
+	PersonRepository prepo;
+	
 }
